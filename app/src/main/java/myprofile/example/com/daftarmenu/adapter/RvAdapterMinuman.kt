aa604fccfc12dab01c85a777d787d8a3abc1b8f2
shofi nurfaldini <shofinurfaldini@gmail.com>
2019-09-29 10:37:38 +0700

    Daftar Menu List

---
+++ app/src/main/java/myprofile/example/com/daftarmenu/adapter/RvAdapterMinuman.kt
@@ -0,0 +1,53 @@
+package myprofile.example.com.daftarmenu.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_list_menu.iv_gambar
+import kotlinx.android.synthetic.main.item_list_menu.tv_harga
+import kotlinx.android.synthetic.main.item_list_menu.tv_mie
+import myprofile.example.com.daftarmenu.R.layout
+import myprofile.example.com.daftarmenu.adapter.RvAdapterMakanan.MenuViewHolder
+import myprofile.example.com.daftarmenu.data.MenuMinumanModel
+import myprofile.example.com.daftarmenu.data.MenuModel
+
+class RvAdapterMinuman(private val data:List<MenuMinumanModel>) :
+    RecyclerView.Adapter<RvAdapterMinuman.MenuViewHolder> () {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        return MenuViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(layout.item_list_menu, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        holder.bindData(data[position])
+    }
+
+    class MenuViewHolder (
+        override val containerView: View)
+        :RecyclerView.ViewHolder(containerView)
+        ,LayoutContainer
+    {
+        fun bindData(item: MenuMinumanModel){
+            tv_mie.text=item.namaMenu
+            tv_harga.text=item.hargaMenu
+            Glide.with(containerView)
+                .load(item.gambarMenu)
+                .into(iv_gambar)
+
+            itemView.setOnClickListener{
+                Toast.makeText(containerView.context,
+                    item.namaMenu,Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/myprofile/example/com/daftarmenu/data/MenuMinumanModel.kt
@@ -0,0 +1,16 @@
+package myprofile.example.com.daftarmenu.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class MenuMinumanModel (
+    @PrimaryKey(autoGenerate = true)
+    var idMenuModel: Int,
+    var namaMenu:String,
+    var hargaMenu:String,
+    @ColumnInfo(typeAffinity = ColumnInfo.BLOB)
+    var gambarMenu:Byte
+)
+
